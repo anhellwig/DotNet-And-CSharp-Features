4bb82cb6f34e000996e61bbbf35dc92a957099d5
Ansgar Hellwig <code@ahellwig.eu>
2025-11-08 15:32:15 +0100

    Add example for simple lambda parameters with modifiers

---
+++ CSharp/CS14/Lambda parameters with modifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+// Previously, adding a modifier like 'out' to a lambda parameter required explicit types for all parameters:
+TryParseInvariant<double> parse2 = (string text, out double result) =>
+    double.TryParse(text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out result);
+
+// Now, with C# 14, you can omit the types of other parameters when using modifiers:
+TryParseInvariant<double> parse1 = (text, out result) =>
+    double.TryParse(text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out result);
+
+if (parse1("4.2", out double i1))
+{
+    Console.WriteLine(i1);
+}
+
+if (parse2("4.2", out double i2))
+{
+    Console.WriteLine(i2);
+}
+
+return 0;
+
+delegate bool TryParseInvariant<T>(string text, out T result);
---
+++ CSharp/CS14/Lambda parameters with modifiers/README.md
@@ -0,0 +1,16 @@
+﻿# Simple lambda parameters with modifiers
+
+This example demonstrates how to use parameter modifiers such as `out` in lambda expressions without specifying the parameter type.
+
+## Example
+
+```csharp
+delegate bool TryParseInvariant<T>(string text, out T result);
+
+TryParseInvariant<double> parse1 = (text, out result) =>
+    double.TryParse(text, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out result);
+```
+
+## Further information
+
+* [Input parameters of a lambda expression](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions#input-parameters-of-a-lambda-expression)
