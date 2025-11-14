3c5c06c73c0305c693b592625239b3bf979a11a5
Ansgar Hellwig <code@ahellwig.eu>
2025-11-14 13:31:09 +0100

    Add examples for collection expressions

---
+++ CSharp/CS12/Collection expressions/Program.cs
@@ -0,0 +1,29 @@
+﻿// Traditional collection initialization:
+
+using System.Collections.ObjectModel;
+
+int[] oldArray = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+List<double> oldList = new() { 1.0, 2.0, 3.0 };
+
+
+// C# 12 introduces a new terse syntax, which works for arrays, Span,
+// interfaces like IEnumerable<T>, and types that supports a collection initializer:
+
+int[] array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+List<double> list = [42.0];
+
+ReadOnlyCollection<string> collection = ["Hello", "world"];
+
+Span<char> span = ['H', 'e', 'l', 'l', 'o', '!'];
+
+PrintElements(['H', 'e', 'l', 'l', 'o', '!']);
+
+int[] a1 = [1, 2, 3];
+int[] a2 = [4, 5, 6];
+int[][] jaggedArray = [a1, a2];
+int[] concatenatedArray = [.. a1, .. a2]; // with spread element '..'
+
+
+static void PrintElements<T>(IEnumerable<T> elements) => Console.WriteLine(string.Join("; ", elements));
---
+++ CSharp/CS12/Collection expressions/README.md
@@ -0,0 +1,17 @@
+﻿# Collection expressions
+
+C# 12 introduces a terse syntax to create common collection values, which works for arrays, Span, interfaces like IEnumerable<T>,
+and types that supports a collection initializer:
+
+## Example
+
+```csharp
+IEnumerable<int> Vaylues => [1, 2, 3, 4, 5];
+ReadOnlySpan<char> span = ['H', 'e', 'l', 'l', 'o', '!'];
+PrintElements(['H', 'e', 'l', 'l', 'o', '!']);
+```
+
+## Further information
+
+* [Collection expressions](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/collection-expressions)
+* [Collection initializers](https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers#collection-initializers)
