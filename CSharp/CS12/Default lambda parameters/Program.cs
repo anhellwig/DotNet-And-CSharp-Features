34a12f2c74be8cac4f2a45bb1bca568d2b65a90e
Ansgar Hellwig <code@ahellwig.eu>
2025-11-14 13:49:39 +0100

    Add examples for default lambda parameters

---
+++ CSharp/CS12/Default lambda parameters/Program.cs
@@ -0,0 +1,12 @@
+﻿// Lambda expressions can have optional parameters or a params array in C# 12.
+
+var isLongerThan = static (string text, int limit = 7) => text.Length > limit;
+Console.WriteLine(isLongerThan("Hello, World!", 7));
+
+// use either 'var' or define a delegate type that includes default parameter values
+SomeIntNumbersDelegate sumSquared = static (params int[] values) => values.Sum(x => x * x);
+Console.WriteLine(sumSquared(1, 2, 3));
+
+return 0;
+
+delegate int SomeIntNumbersDelegate(params int[] values);
---
+++ CSharp/CS12/Default lambda parameters/README.md
@@ -0,0 +1,14 @@
+﻿# Default lambda parameters
+
+In C# 12, you can declare a lambda expression with default parameters or with `params` array.
+
+## Example
+
+```csharp
+var isLongerThan = (string text, int limit = 7) => text.Length > limit;
+var sumSquared = (params int[] values) => values.Sum(x => x * x);
+```
+
+## Further information
+
+* [Input parameters of a lambda expression](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions#input-parameters-of-a-lambda-expression)
