a3f85b9305cbfc34a6bc42fe81a864ec2a906dd7
Ansgar Hellwig <code@ahellwig.eu>
2025-11-14 12:07:26 +0100

    Add example for generic attributes

---
+++ CSharp/CS11/Generic attributes/MyCustomAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericAttributes;
+
+// technique to be used in previous version of C#, utilizing a constructor parameter:
+[AttributeUsage(AttributeTargets.Method)]
+public class MyCustomAttribute : Attribute
+{
+    public MyCustomAttribute(Type t) => ParamType = t;
+    public Type ParamType { get; }
+}
+
+// C# 11 generic attributes feature:
+[AttributeUsage(AttributeTargets.Method)]
+public class MyCustomGenericAttribute<T> : Attribute { }
+
+public class ExampleConverter
+{
+    [MyCustom(typeof(string))]
+    [MyCustomGeneric<string>]
+    public static string Method() => Guid.NewGuid().ToString();
+}
---
+++ CSharp/CS11/Generic attributes/README.md
@@ -0,0 +1,14 @@
+﻿# Generic attributes
+
+C# 11 introduces support for generic attributes, allowing you to define attributes that can take type parameters.
+
+## Example
+
+```csharp
+class MyCustomGenericAttribute<T> : Attribute { }
+```
+
+## Further information
+
+* [Generic attributes](https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-11#generic-attributes)
+* [Extend metadata using attributes](https://learn.microsoft.com/en-us/dotnet/standard/attributes/)
