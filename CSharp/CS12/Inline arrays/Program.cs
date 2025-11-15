0226c6081e554bb85f6621f3866d63a5ae2ccd15
Ansgar Hellwig <code@ahellwig.eu>
2025-11-15 11:25:00 +0100

    Add example for inline arrays

---
+++ CSharp/CS12/Inline arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    const int bufferLength = 10;
+
+    private static int Main()
+    {        
+        IntBuffer buffer = new();
+
+        for (int i = 0; i < bufferLength; i++)
+        {
+            buffer[i] = i;
+        }
+
+        foreach (int i in buffer)
+        {
+            Console.WriteLine(i);
+        }
+
+        return 0;
+    }
+
+    [System.Runtime.CompilerServices.InlineArray(bufferLength)]
+    internal struct IntBuffer
+    {
+        // The struct must contain a single field.
+        // The type can be any value type or reference type, but not a pointer.
+        private int numbers;
+
+        // equivalent to fixed-size buffer, but that world require unsafe context:
+        //public fixed int fixedBuffer[bufferLength]; 
+    }
+}
---
+++ CSharp/CS12/Inline arrays/README.md
@@ -0,0 +1,18 @@
+﻿# Inline arrays
+
+Inline arrays allow you to define a fixed-size array directly within a struct, without needing an unsafe context.
+They might be utilized e.g. by library authors in high-performance scenarios.
+
+## Example
+
+```csharp
+[System.Runtime.CompilerServices.InlineArray(42)]
+public struct IntBuffer
+{
+    private int numbers;
+}
+```
+
+## Further information
+
+* [Inline arrays](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/struct#inline-arrays)
